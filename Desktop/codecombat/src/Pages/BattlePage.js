0225698155fa5f1ed3f036d78b4e06fa4e08651b
Pragnan143 <pragnanmanam8@gmail.com>
2024-11-13 02:58:43 +0530

    Skleton Update

---
+++ Desktop/codecombat/src/Pages/BattlePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../Components/Navbar/Navbar'
+
+const BattlePage = () => {
+  return (
+    <div>
+        <Navbar/>
+    <div className='flex items-center justify-center text-4xl'>
+        Page Under Maintance   
+    </div>
+    </div>
+  )
+}
+
+export default BattlePage
